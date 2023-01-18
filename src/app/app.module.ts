23be726892283beee7fc412cb0ec754cea9fdbc6
dsfyer <Barbatomateo@gmail.com>
2023-01-18 15:17:33 -0400

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { ContentComponent } from './components/content/content.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
